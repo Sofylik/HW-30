cc0334698f7f24a7e046a0d68f26b8b99a34b00b
Sofylik <sofiasemciv@gmail.com>
2024-10-09 16:51:41 +0300

    000
    098789

---
+++ index.js
@@ -0,0 +1,32 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+const dots = document.querySelectorAll('.line');
+
+
+function updateButtons() {
+    prevBtn.disabled = currentSlide === 0;
+    nextBtn.disabled = currentSlide === slides.length - 1;
+    dots.forEach((dot, index) => {
+        dot.classList.toggle('active', index === currentSlide);
+    });
+
+}
+
+prevBtn.addEventListener('click', () => {
+    let number = -1;
+    slides[currentSlide].classList.remove('active');
+    currentSlide += number;
+    slides[currentSlide].classList.add('active');
+    updateButtons();
+});
+
+nextBtn.addEventListener('click', () => {
+    let number = 1;
+    slides[currentSlide].classList.remove('active');
+    currentSlide += number;
+    slides[currentSlide].classList.add('active');
+    updateButtons();
+});
+updateButtons();
